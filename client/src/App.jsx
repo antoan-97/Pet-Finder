93b6af75f3c1d14bb8f440d7ddfb9e6bb96fa008
antoan-97 <antoan.todorov@abv.bg>
2024-08-31 16:09:23 +0300

    Add static Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 
   return (
     <div className='bg-green-500' >
+          <Navbar />
+
       <h1 className="text-3xl fond-bold">Welcome to PetFinder</h1>
     </div>
   )
