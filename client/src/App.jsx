0b172fc76df4a137a05f6b5597a20908de61e6a7
antoan-97 <antoan.todorov@abv.bg>
2024-09-12 14:21:41 +0300

    Add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
 import Home from './components/home/Home'
 import Navbar from './components/navbar/Navbar'
+import AboutUs from './components/aboutus/Aboutus'
 
 function App() {
 
   return (
-    <div className='bg-green-500' >
-          <Navbar />
-          <Home />
+    <div className='bg-blue-300' >
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about-us' element={<AboutUs />}></Route>
+      </Routes>
+
+      <Footer />
     </div>
   )
 }
