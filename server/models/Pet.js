f677200410869536382e1cbc69ac358e9b996f57
antoan-97 <antoan.todorov@abv.bg>
2024-09-12 12:15:59 +0300

    Create mongoose pet model schema

---
+++ server/models/Pet.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const petSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    breed: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: false,
+    },
+    description: String,
+    imgUrl: String,
+    adopted: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const Pet = mongoose.model('Pet', petSchema)
+module.exports = Pet
