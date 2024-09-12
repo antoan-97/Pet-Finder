167f48331d000548807816e4ba7f7149b504a3cb
antoan-97 <antoan.todorov@abv.bg>
2024-09-12 15:25:14 +0300

    Add dynamic Navbar and Footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,71 @@
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+    return (
+        <>
+            <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+            />
+            <footer className="bg-blue-400 text-white py-8">
+                <div className="container mx-auto px-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                        <div>
+                            <h4 className="text-lg font-bold mb-4">PawFinder</h4>
+                            <p>
+                                PawFinder is your dedicated platform for finding and adopting homeless dogs and cats.
+                            </p>
+                        </div>
+                        <div>
+                            <h4 className="text-lg font-bold mb-4">Quick Links</h4>
+                            <ul className="space-y-2">
+                                <li>
+                                    <a href="#" className="hover:text-gray-300">
+                                        Our Animals
+                                    </a>
+                                </li>
+                                <li>
+                                    <Link to='about-us' className="hover:text-gray-300">
+                                        About us
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to='/donate' className="hover:text-gray-300">
+                                        Support Us
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h4 className="text-lg font-bold mb-4">Contact Us</h4>
+                            <ul className="space-y-2">
+                                <li>
+                                    <a href="mailto:pawfinder@abv.bg" className="hover:text-gray-300">
+                                        pawfinder@abv.bg
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div>
+                            <h4 className="text-lg font-bold mb-4">Follow Us</h4>
+                            <div className="flex space-x-4">
+                                <a href="https://www.facebook.com/groups/1472732316295842" className="hover:text-pink-500">
+                                    <i className="fab fa-facebook fa-lg" />
+                                </a>
+
+                                <a href="https://www.instagram.com/four_paws_international/?hl=bg" className="hover:text-pink-500">
+                                    <i className="fab fa-instagram fa-lg" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-center mt-8">
+                        <p>© 2024 PawFinder. All rights reserved.</p>
+                    </div>
+                </div>
+            </footer>
+        </>
+
+    );
+}
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,93 +1,90 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a
-                href="https://flowbite.com/"
-                className="flex items-center space-x-3 rtl:space-x-reverse"
-            >
-                <img
-                    src="images/logo.png"
-                    className="h-12 w-auto" // Adjusted size for better responsiveness
-                    alt="Flowbite Logo"
-                />
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                    PetFinder
-                </span>
-            </a>
-            <button
-                data-collapse-toggle="navbar-default"
-                type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-default"
-                aria-expanded="false"
-            >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 17 14"
+        <nav className="bg-white border-blue-400 dark:bg-blue-400">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link to='/'
+                    className="flex items-center space-x-3 rtl:space-x-reverse"
                 >
-                    <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M1 1h15M1 7h15M1 13h15"
+                    <img
+                        src="images/logo2.jpg"
+                        className="h-12 w-auto"
+                        alt="Flowbite Logo"
                     />
-                </svg>
-            </button>
-            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li>
-                        <a
-                            href="#"
-                            className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                            aria-current="page"
-                        >
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                        >
-                            About Us
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                        >
-                            Find Your Pet
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                        >
-                            Donate
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                        >
-                            Contact
-                        </a>
-                    </li>
-                </ul>
+                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                        PawFinder
+                    </span>
+                </Link>
+                <button
+                    onClick={toggleMenu}
+                    data-collapse-toggle="navbar-default"
+                    type="button"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-default"
+                    aria-expanded={isOpen ? 'true' : 'false'}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 17 14"
+                    >
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M1 1h15M1 7h15M1 13h15"
+                        />
+                    </svg>
+                </button>
+                <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-blue-400 md:dark:bg-blue-400 dark:border-blue-400">
+                        <li>
+                            <Link to='/'
+
+                                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-white"
+                                aria-current="page"
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/about-us'
+                                href="#"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >
+                                About Us
+                            </Link>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >
+                                Find Your Pet
+                            </a>
+                        </li>
+                        <li>
+                            <Link to='/donate'
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                            >
+                                Donate
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-
-
+        </nav>
     );
 }
