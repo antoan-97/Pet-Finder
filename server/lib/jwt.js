31aa36c77387b6cafc632ae5c854dcbd0e52548d
antoan-97 <antoan.todorov@abv.bg>
2024-09-13 15:00:31 +0300

    Create jsonwebtoken

---
+++ server/lib/jwt.js
@@ -0,0 +1,10 @@
+const { sign, verify } = require('crypto');
+const jsonwebtoken = require('jsonwebtoken');
+const util = require('util');
+
+const jwt = {
+    sign: util.promisify(jsonwebtoken.sign),
+    verify: util.promisify(jsonwebtoken.verify)
+};
+
+module.exports = jwt
