396b6fd09727d9e846a2d84c544f04555e260ae5
antoan-97 <antoan.todorov@abv.bg>
2024-09-30 14:40:27 +0300

    Add api services

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,40 @@
 import axios from 'axios'
 
-export const getAllPets = async () => {
+const baseUrl = import.meta.env.VITE_BASE_URL;
+//Register
+export const register = async (email, password) => {
     try {
-        const response = await axios.get('/api');
+        const response = await axios.post(`${baseUrl}/api/users/register`, {
+            email,
+            password,
+        });
         return response.data
     } catch (error) {
-        console.error('Error fetching pets:', error);
+        console.error('Error registering user:', error);
         throw error;
     }
-}
+};
+
+// Login
+export const login = async (email, password) => {
+    try {
+        const response = await axios.post(`${baseUrl}/api/users/login`, {
+            email,
+            password,
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error logging in:', error);
+        throw error;
+    }
+};
+
+// export const getAllPets = async () => {
+//     try {
+//         const response = await axios.get('/api');
+//         return response.data
+//     } catch (error) {
+//         console.error('Error fetching pets:', error);
+//         throw error;
+//     }
+// }
