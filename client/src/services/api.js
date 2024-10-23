41c3e9056e8ec305a3c6e2142488c087735b04ad
antoan-97 <antoan.todorov@abv.bg>
2024-10-23 12:32:53 +0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,14 +1,13 @@
 const baseURL = import.meta.env.VITE_BASE_URL + '/api';
 import Cookies from 'js-cookie';
 
-const getToken = () => {
+export const getToken = () => {
     const token = Cookies.get('accessToken');
-    console.log('Retrieved token:', token); // Log the token for debugging
+    console.log('Retrieved token:', token);
     return token;
 };
 
-
-//Register
+// User-related API calls (register, login, logout) stay here
 export const register = async (email, password) => {
     const token = getToken();
     console.log('Base URL:', baseURL); // Debugging line
@@ -33,7 +32,7 @@ export const register = async (email, password) => {
 
     return data
 }
-// Login
+
 export const login = async (email, password) => {
     const token = getToken(); // 
     console.log('Base URL:', baseURL); // Debugging line
@@ -41,7 +40,7 @@ export const login = async (email, password) => {
         method: 'POST',
         headers: {
             'content-type': 'application/json',
-            'Authorization': `Bearer ${token}`, 
+            'Authorization': `Bearer ${token}`,
             'Accept': 'application/json'
         },
         body: JSON.stringify({
@@ -57,7 +56,7 @@ export const login = async (email, password) => {
 
     const data = await response.json();
 
-    return data; 
+    return data;
 };
 
 export const logout = async () => {
@@ -93,46 +92,6 @@ export const logout = async () => {
     }
 }
 
-export const addFoundPet = async (formData) => {
-    const token = getToken();
-
-    const form = new FormData();
-    form.append('kind', formData.kind);
-    form.append('location', formData.location);
-    form.append('breed', formData.breed);
-    form.append('phone', formData.phone);
-    form.append('description', formData.description);
-    form.append('image', formData.image);
-
-    try {
-        const response = await fetch(`${baseURL}/pets/addFoundPet`, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-            },
-            body: form,
-        });
-
-        if (!response.ok) {
-            const error = await response.json();
-            throw new Error(error.message || 'Failed to submit found pet information');
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error while submitting found pet:', error);
-        throw error;
-    }
-};
 
 
-// export const getAllPets = async () => {
-//     try {
-//         const response = await axios.get('/api');
-//         return response.data
-//     } catch (error) {
-//         console.error('Error fetching pets:', error);
-//         throw error;
-//     }
-// }
+
