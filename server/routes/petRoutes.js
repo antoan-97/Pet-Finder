1cff0be1ea1a57030a4e4ffbd03d7514e43dd5b6
Antoan Todorov <antoan.todorov97@gmail.com>
2024-10-30 10:51:09 +0200

    Update petRoutes.js

--- server/routes/petRoutes.js
+++ server/routes/petRoutes.js
@@ -5,7 +5,9 @@ const upload = require('../middlewares/multer');
 const router = express.Router();
 
 router.post('/addFoundPet', upload.single('image'), petController.addFoundPet);
-router.get('/', petController.getAllPets);
+router.post('/addLostPet', upload.single('image'), petController.addLostPet);   
+router.get('/found', petController.getAllPets);
+router.get('/lost', petController.getAllLost);
 router.get('/:id', petController.getOnePet);
 
 module.exports = router;
