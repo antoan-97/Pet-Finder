277bebad029c1bbc534017a95c7a5981a32b3b5e
Antoan Todorov <antoan.todorov97@gmail.com>
2024-12-25 17:11:49 +0200

    Update width on catalogs

--- client/src/components/adoptPet/dogAdoption/DogAdoptionList.jsx
+++ client/src/components/adoptPet/dogAdoption/DogAdoptionList.jsx
@@ -44,7 +44,7 @@ export default function FoundPetsList() {
         )}
 
         <div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-3/4">
             {pets.length > 0 ? (
               pets.map(pet => (
                 <DogAdoptionCard key={pet._id} {...pet} />
