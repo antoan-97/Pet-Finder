f4521188c23a8b3a64daa96f90e640d3cb70a109
Antoan Todorov <antoan.todorov97@gmail.com>
2025-01-21 21:05:50 +0200

    Implement found pet edit router

--- server/routes/petRoutes.js
+++ server/routes/petRoutes.js
@@ -8,11 +8,13 @@ const router = express.Router();
 router.post('/addLostPet', upload.single('image'), petController.addLostPet);   
 router.get('/lost/:id', petController.getOneLost);
 router.get('/lost', petController.getAllLost);
+router.delete('/lost/:id', petController.deleteLostPet);
 //found pets
 router.post('/addFoundPet', upload.single('image'), petController.addFoundPet);
 router.get('/found', petController.getAllFound);
 router.get('/found/:id', petController.getOneFound);
 router.delete('/found/:id', petController.deleteFoundPet);
-router.delete('/lost/:id', petController.deleteLostPet);
+router.put('/found/:id', petController.updateFoundPet);
+
 
 module.exports = router;
