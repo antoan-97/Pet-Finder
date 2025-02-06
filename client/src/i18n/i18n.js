9d9fcfe48754d4f889065447c8f99642e78e3e6c
Antoan Todorov <antoan.todorov97@gmail.com>
2025-02-06 21:54:06 +0200

    Implement i18n library

---
+++ client/src/i18n/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationBG from './locales/bg/translation.json';
+import translationEN from './locales/en/translation.json';
+
+
+const resources = {
+    bg: {
+        translation: translationBG
+    },
+    en: {
+        translation: translationEN
+    }
+};
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: 'bg', // default language
+        fallbackLng: 'en', // fallback language
+        interpolation: {
+            escapeValue: false // react already safes from xss
+        }
+    });
+
+
+export default i18n;
