d7e714659dbcd7a3604978a928d3405928dfbe79
Antoan Todorov <antoan.todorov97@gmail.com>
2025-03-17 22:11:19 +0200

    Add back-end validations for Found Pets form

--- server/models/FoundPet.js
+++ server/models/FoundPet.js
@@ -4,18 +4,26 @@ const petSchema = new mongoose.Schema({
     kind: {
         type: String,
         required: true,
+        minlength: 3,
+        maxlength: 20
     },
     location: {
         type: String,
         required: true,
+        minlength: 3,
+        maxlength: 20
     },
     breed: {
         type: String,
         required: true,
+        minlength: 3,
+        maxlength: 20
     },
     phone: {
         type: String,
         required: true,
+        minlength: 3,
+        maxlength: 20
     },
     description: String,
     imgUrl: String,
